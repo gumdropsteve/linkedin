b47eb45ed6bc57bf627b809f8bf5578e00d45139
Winston <winston@Winstons-MacBook-Pro.local>
2019-05-21 14:47:59 -0700

    from paths.py; HE

---
+++ info.py
@@ -0,0 +1,14 @@
+# linkedin login page 
+login_page = 'https://www.linkedin.com/login'
+# xpath of 'email or phone' input box
+user_box = '//*[@id="username"]'
+# xpath of 'password' input box
+pass_box = '//*[@id="password"]'
+# css selector of 'start a post' button
+start_share_button = '.share-box__open'
+# css selector of post input box
+to_talk_about = '.mentions-texteditor__contenteditable' 
+# xpath of 'post' button
+post_button = '//*[contains(@class,"share-actions__primary-action")]'
+# known link types
+the_links = ['.com','.pdf','.org','.net','.gov','.io']
